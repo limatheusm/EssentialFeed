f0d4a7be69e339f01ccf3ed88a7a297cbfe7da31
limatheusm <limatheusm@gmail.com>
2022-10-06 22:22:57 -0300

    class #4: Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,20 +7,34 @@
 
 import Foundation
 
+public typealias HTTPClientResult = Result<HTTPURLResponse, Error>
+
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteFeedLoader {
     private let client: HTTPClient
     private let url: URL
 
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+
     public init(client: HTTPClient, url: URL) {
         self.client = client
         self.url = url
     }
 
-    public func load() {
-        client.get(from: url)
+    public func load(completion: @escaping (Error) -> Void) {
+        client.get(from: url, completion: { result in
+            switch result {
+            case .success:
+                completion(.invalidData)
+            case .failure:
+                completion(.connectivity)
+            }
+        })
     }
 }
