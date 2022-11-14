aeca4104b9e56ca8c6b7fb003597330bf0946ce9
limatheusm <limatheusm@gmail.com>
2022-11-14 20:30:55 -0300

    class: Test-driving Cache Invalidation + Identifying Complex (Bloated) Functionality With The Command–Query Separation Principle

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -39,10 +39,14 @@ public final class LocalFeedLoader {
 
             switch result {
             case let .failure(error):
+                self.store.deleteCachedFeed { _ in }
                 completion(.failure(error))
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
-            case .found, .empty:
+            case .found:
+                self.store.deleteCachedFeed { _ in }
+                completion(.success([]))
+            case .empty:
                 completion(.success([]))
             }
         }
