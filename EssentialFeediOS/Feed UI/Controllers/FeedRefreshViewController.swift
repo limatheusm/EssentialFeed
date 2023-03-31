1b680598213547087df06b069ebb83a1b92887ca
limatheusm <limatheusm@gmail.com>
2023-03-30 22:46:16 -0300

    class: Refactoring Massive View Controllers Into Multiple Tiny MVCs + Using Composers & Adapters to Reduce Coupling and Eliminate Redundant Dependencies

---
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FeedRefreshViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Matheus Lima on 3/30/23.
+//
+
+import EssentialFeed
+import UIKit
+
+final class FeedRefreshViewController: NSObject {
+    private(set) lazy var view: UIRefreshControl = {
+        let view = UIRefreshControl()
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
+    }()
+
+    private let feedLoader: FeedLoader
+
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+
+    var onRefresh: (([FeedImage]) -> Void)?
+
+    @objc func refresh() {
+        view.beginRefreshing()
+        feedLoader.load(completion: { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onRefresh?(feed)
+            }
+
+            self?.view.endRefreshing()
+        })
+    }
+}
