e37ffde4e65e4032cb179fe7ad4ae5f3b28752d7
limatheusm <limatheusm@gmail.com>
2023-07-30 23:17:08 -0300

    Composing Modules Into a Running iOS Application with Xcode Workspaces

---
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SceneDelegate.swift
+//  EssentialApp
+//
+//  Created by Matheus Lima on 7/30/23.
+//
+
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+
+        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
+        let feedLoader = RemoteFeedLoader(client: client, url: url)
+        let imageLoader = RemoteFeedImageDataLoader(client: client)
+
+        let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: feedLoader, imageLoader: imageLoader)
+        window?.rootViewController = feedViewController
+        window?.makeKeyAndVisible()
+    }
+}
+
